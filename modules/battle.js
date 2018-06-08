e3be60aa4057ca49de1f1565302f4a7447a3a064
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 21:45:40 +0800

    Update battle.js

--- modules/battle.js
+++ modules/battle.js
@@ -3,5 +3,26 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	  var name = file.replace('.js', '');
 	  exports[name] = require('../roll/' + file);
 	}
-  });
-  
+});
+var battle;
+
+function parseInput(inputStr,UserID,UserN) {
+	//console.log('InputStr: ' + inputStr);
+	_isNaN = function(obj) 	{
+	return isNaN(parseInt(obj));  
+	}
+	
+	let msgSplitor = (/\S+/ig);	
+	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
+	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
+	
+///////////////////////////////////////////////////////
+////////////////////////開始戰鬥////////////////////////
+///////////////////////////////////////////////////////
+	
+	return battle;
+}
+
+module.exports = {
+	parseInput:parseInput
+};
