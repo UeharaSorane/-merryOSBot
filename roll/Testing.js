07c1e3c1b02ccede9023a9892c05d6f9b0b3a15d
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 22:25:43 +0800

    Update Testing.js

--- roll/Testing.js
+++ roll/Testing.js
@@ -3,9 +3,9 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-	channelId: '1568211787',
-	channelSecret: '9848b4e4a814dd07e84212da82bad4a3',
-	channelAccechannelAccessToken: '68lsiTjqwl30Ff6Ik5NH7KYW4FGyRsZYV+NZcVsS85CU3o+QaiLETQNZgaZwYNKkdX71iUjw4pz1OfJCkJeovBzfvF2Eoit0anKv9/Mxxd6OEY06r20Ad6Lxi3rdadSKqE8khBgJZUYxQDbLndtoBQdB04t89/1O/w1cDnyilFU=',
+	channelId: "1568211787",
+	channelSecret: "9848b4e4a814dd07e84212da82bad4a3",
+	channelAccessToken: "68lsiTjqwl30Ff6Ik5NH7KYW4FGyRsZYV+NZcVsS85CU3o+QaiLETQNZgaZwYNKkdX71iUjw4pz1OfJCkJeovBzfvF2Eoit0anKv9/Mxxd6OEY06r20Ad6Lxi3rdadSKqE8khBgJZUYxQDbLndtoBQdB04t89/1O/w1cDnyilFU=",
 });
 
 function ReplyTest(UserN,myText) {
