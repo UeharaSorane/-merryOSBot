a414800da26b3b429ef8be3339462865b3368a33
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 22:11:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('fs').readdirSync(__dirname + '/modules/').forEach(function(file) {
 var bot = linebot({
 	channelId: '1568211787',
 	channelSecret: '9848b4e4a814dd07e84212da82bad4a3',
-	channelAccechannelAccessToken: '68lsiTjqwl30Ff6Ik5NH7KYW4FGyRsZYV+NZcVsS85CU3o+QaiLETQNZgaZwYNKkdX71iUjw4pz1OfJCkJeovBzfvF2Eoit0anKv9/Mxxd6OEY06r20Ad6Lxi3rdadSKqE8khBgJZUYxQDbLndtoBQdB04t89/1O/w1cDnyilFU=',
+	channelAccechannelAccessToken: '68lsiTjqwl30Ff6Ik5NH7KYW4FGyRsZYV+NZcVsS85CU3o+QaiLETQNZgaZwYNKkdX71iUjw4pz1OfJCkJeovBzfvF2Eoit0anKv9/Mxxd6OEY06r20Ad6Lxi3rdadSKqE8khBgJZUYxQDbLndtoBQdB04t89/1O/w1cDnyilFU='
 });
 
 var app = express();
