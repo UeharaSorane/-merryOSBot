61a3248456bc203ee2df9f0fa5ad18f2d9d5d12f
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 19:20:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var linebot = require('linebot');
+var express = require('express');
 
 var bot = linebot({
   channelId: process.env.LINE_CHANNEL_ID,
@@ -6,15 +7,16 @@ var bot = linebot({
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN
 });
 
-console.log('斷');
+var app = express();
+var linebotParser = bot.parser();
+app.post('/', linebotParser());
 
-
-bot.on('message', function (event) {
-  event.reply(event.message.text).then(function (data) {
-    // success
-  }).catch(function (error) {
-    // error
-  });
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
 
-bot.listen('/linewebhook', 3000);
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
