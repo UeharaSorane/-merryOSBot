4665a1e70c69a7c6e8166962e445c0ae78ac818e
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 20:47:24 +0800

    Create analytics.js

---
+++ modules/analytics.js
@@ -0,0 +1,18 @@
+require('fs').readdirSync('./roll/').forEach(function(file) {
+	if (file.match(/\.js$/) !== null && file !== 'index.js') {
+	  var name = file.replace('.js', '');
+	  exports[name] = require('../roll/' + file);
+	}
+  });
+  
+  function parseInput(rplyToken, inputStr,UserID,Dname) {
+	//console.log('InputStr: ' + inputStr);
+	_isNaN = function(obj) 	{
+	return isNaN(parseInt(obj));  
+	}
+	
+	let msgSplitor = (/\S+/ig);	
+	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
+	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
+	
+}
