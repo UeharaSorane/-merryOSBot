2243bbebdda2ff3d0132f7280cee74c7a14a4d64
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 20:53:07 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -5,7 +5,7 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	}
   });
   
-  function parseInput(rplyToken, inputStr,UserID,Dname) {
+function parseInput(rplyToken, inputStr,UserID,UserN) {
 	//console.log('InputStr: ' + inputStr);
 	_isNaN = function(obj) 	{
 	return isNaN(parseInt(obj));  
@@ -15,4 +15,14 @@ require('fs').readdirSync('./roll/').forEach(function(file) {
 	let mainMsg = inputStr.match(msgSplitor); //定義輸入字串
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 	
+///////////////////////////////////////////////////////
+////////////////////////開始分析////////////////////////
+///////////////////////////////////////////////////////
+	
+////////////////////////測試用
+	if (trigger.match(/^鸚鵡測試$/) != null) return exports.Testing.ReplyTest(UserN,mainMsg[1]);
 }
+
+module.exports = {
+	parseInput:parseInput
+};
