187a33768bb82c5953a22c857d4e7c94c0b351aa
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 21:04:43 +0800

    Update Testing.js

--- roll/Testing.js
+++ roll/Testing.js
@@ -1,15 +1,19 @@
 function ReplyTest(UserN,myText) {
 	///確認系統reply功能沒問題用
+	var rply;
       
-if(mainMsg[1] == null){
-				rply = UserN + '你啥都沒說啊...';
-			}else{
-				rply = UserN + '你剛剛說了：' + mainMsg[1];
-			}
- }
+	if(myText == null){
+		rply = UserN + '\n你啥都沒說啊...';
+	}else{
+		rply = UserN + '你剛剛說了：' + myText;
+	}
 				
 	return rply;
 	
 	///
 
 }
+
+module.exports = {
+	ReplyTest
+};
