770ceb865f716c43f087ed11872f21e791aa6cdf
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-08 19:23:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var bot = linebot({
 });
 
 var app = express();
-app.post('/', bot.parser();
+app.post('/', bot.parser());
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen(process.env.PORT || 8080, function() {
