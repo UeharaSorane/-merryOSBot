3101ee054aa998eb8311a3535e00813655923a62
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-09 15:31:21 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -23,7 +23,7 @@ function parseInput(inputStr,UserID,UserN,GroupID) {
 ////////////////////////測試用
 	if (trigger.match(/^鸚鵡測試$/) != null) return exports.Testing.ReplyTest(UserN,mainMsg[1]);//測試reply功能用
 	if (trigger.match(/^大聲測試$/) != null) return exports.Testing.PushTest(UserID,GroupID);//測試push功能用
-	if (trigger.match(/^戰鬥系統測試$/) != null) return exports.Testing.SimBattle(UserID,GroupID);//測試開啟戰鬥系統功能用
+	if (trigger.match(/^戰鬥系統測試$/) != null) return exports.Testing.SimBattle(UserID,GroupID,UserN);//測試開啟戰鬥系統功能用
 }
 
 module.exports = {
