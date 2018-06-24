1e318d34fc9afaa2f16481880d565f92d4e23950
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-25 00:49:23 +0800

    Update battleUI.js

--- battlesys/battleUI.js
+++ battlesys/battleUI.js
@@ -2,7 +2,7 @@
 ////////////////此區設計玩家及敵人資料////////////////
 ////////////////////////////////////////////////////
 
-function BattleInfo(ID,UName,CName,Team,Hp,Mp,Atk,spd,Ability,WS,AS,BS,S1,S2,S3,MS,MStype,Impact){
+function BattleInfo(ID,UName,CName,Team,Hp,Mp,Atk,spd,Ability,WS,AS,BS,Skill,MS,MStype,Impact){
   this.ID = ID;
   this.UName = UName;
   this.CName = CName;
@@ -15,9 +15,7 @@ function BattleInfo(ID,UName,CName,Team,Hp,Mp,Atk,spd,Ability,WS,AS,BS,S1,S2,S3,
   this.WS = WS;
   this.AS = AS;
   this.BS = BS;
-  this.S1 = S1;
-  this.S2 = S2;
-  this.S3 = S3;
+  this.Skill = ['','',''];
   this.MS = MS;
   this.MStype = MStype;
   this.Immpact = Impact;
