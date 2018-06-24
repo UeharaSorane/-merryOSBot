5cff6714e785b1849d88179835e2192626dc1df3
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-24 22:43:22 +0800

    Update battleUI.js

--- battlesys/battleUI.js
+++ battlesys/battleUI.js
@@ -1,3 +1,24 @@
 ////////////////////////////////////////////////////
 ////////////////此區設計玩家及敵人資料////////////////
 ////////////////////////////////////////////////////
+
+var BattleInfo = function(ID,Name,Hp,Mp,Atk,Ability,WS,AS,BS,S1,S2,S3,MS,MStype,Impact){
+  this.ID = ID;
+  this.Name = Name;
+  this.Hp = Hp;
+  this.Mp = Mp;
+  this.Atk = Atk;
+  this.Ability = Ability;
+  this.WS = WS;
+  this.AS = AS;
+  this.BS = BS;
+  this.S1 = S1;
+  this.S2 = S2;
+  this.S3 = S3;
+  this.MS = MS;
+  this.MStype = MStype;
+  this.Immpact = Impact;
+  
+};
+
+
