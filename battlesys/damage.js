78ffbe099f9ccdebf51e268b8b17ce0b000283ad
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-26 00:51:03 +0800

    Update damage.js

--- battlesys/damage.js
+++ battlesys/damage.js
@@ -1,45 +1,11 @@
 var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('../client_secret.json');
 var rollbase = require('../roll/rollbase.js');
+var Skill = require('../database/Skill.js');
 
-var SkillDB = new GoogleSpreadsheet('19dmO1oYtwVES_It0rixnrYVojvRMsbz9w5H3yg1Cm9Y');
+var Skills = Skill.getSkillData();
 
-var Skills = [];
-
-SkillDB.useServiceAccountAuth(creds, function (err) {
-		
- 
-	
- // 是先將資料讀進陣列
-	SkillDB.getRows(1 , 
-		function (err, rows) {
-			if (err) {
-				console.log( err );
-			}else{
-				for(var i=0; i< rows.length; i++){
-					Skills[i].Name = rows[i].skillname;
-					Skills[i].Type = rows[i].type;
-					Skills[i].Range = rows[i].range;
-					Skills[i].Plus = rows[i].plus;
-					Skills[i].minRM = rows[i].minrm;
-					Skills[i].maxRM = rows[i].maxrm;
-					Skills[i].SpdM = rows[i].spdm;
-					Skills[i].Priority = rows[i].priority;
-					
-				}
-				console.log(Skills);
-				console.log('技能資料 讀取完成');
-			}
-		
-
-			
-			});
-	
-		
-		
-	});
-
-function damage(move,Atk,Spd,T){
+function damage(Who,move,Atk,Spd,T){
 	var type;
 	
 	for(var i = 0; i<Skills.length;i++){
@@ -52,7 +18,7 @@ function damage(move,Atk,Spd,T){
 				var dmg = Atk*Multiply;
 				var rspd = Spd*Skills[i].SpdM;
 				
-				return type dmg rspd Skills[i].Priority T;
+				return type dmg rspd Skills[i].Priority T Who move;
 			}
 		}
 	}
