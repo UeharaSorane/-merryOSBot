19f0c5f13c46a54ca92ff2e892ebf6dd02e8e3ab
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-26 16:52:43 +0800

    Update battle.js

--- modules/battle.js
+++ modules/battle.js
@@ -50,7 +50,7 @@ function parseInput(inputStr,UserID,UserN,GroupID) {
 	
 	////////////////判定是否在戰鬥
 	if(BTon == 0){
-		return NormalSys.parseInput(mainMsg,trigger,UserID,UserN,GroupID);
+		return NormalSys.NormalMode(mainMsg,trigger,UserID,UserN,GroupID);
 	}
 	////////////////
 	
