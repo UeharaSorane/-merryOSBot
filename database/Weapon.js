3b8bf1db9c4a59f8c049a2ce2357a0cde01cde66
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-27 17:00:57 +0800

    Update Weapon.js

--- database/Weapon.js
+++ database/Weapon.js
@@ -1,1 +1,54 @@
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('../client_secret.json');
 
+var SkillDB = new GoogleSpreadsheet('19dmO1oYtwVES_It0rixnrYVojvRMsbz9w5H3yg1Cm9Y');
+
+var Skills = [];
+
+SkillDB.useServiceAccountAuth(creds, function (err) {
+		
+ 
+	
+ // 是先將資料讀進陣列
+	SkillDB.getRows(1 , 
+		function (err, rows) {
+			if (err) {
+				console.log( err );
+			}else{
+				for(var i=0; i< rows.length; i++){
+					Skills[i] = {};
+					
+					Skills[i].Name = rows[i].skillname;
+					Skills[i].Type = rows[i].type;
+					Skills[i].Mp = Number(rows[i].mp);
+					Skills[i].Range = rows[i].range;
+					Skills[i].Plus = Number(rows[i].plus);
+					Skills[i].minRM = Number(rows[i].minrm);
+					Skills[i].maxRM = Number(rows[i].maxrm);
+					Skills[i].SpdM = Number(rows[i].spdm);
+					Skills[i].Priority = Number(rows[i].priority);
+					Skills[i].Descirption = rows[i].description;
+					Skills[i].W1 = rows[i].W1;
+					Skills[i].W2 = rows[i].W2;
+					Skills[i].W3 = rows[i].W3;
+					
+				}
+				console.log(Skills);
+				console.log('技能資料 讀取完成');
+			}
+		
+
+			
+			});
+	
+		
+		
+	});
+
+function getSkillData(){
+	return Skills;
+}
+
+module.exports = {
+	getSkillData
+};
