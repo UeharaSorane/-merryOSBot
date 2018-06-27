2a73f9a470fd3277544d45270994b106eadf566d
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-27 23:22:40 +0800

    Update battle.js

--- modules/battle.js
+++ modules/battle.js
@@ -673,7 +673,7 @@ function battlesys(command,move,target,commander){
 			for(var sm1 = 0;sm1<Skills.length;sm1++){
 				if(spdl[i][6] == Skills[sm1].Name){
 					for(var sm2 = 0;sm2<info[3].length;sm2++){
-						console.log(1);
+						console.log(info[3][sm2].UName);
 						if(spdl[5] == info[3][sm2].UName){
 							console.log(2);
 							var NeedMp = 0;
