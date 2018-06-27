a862c9b4149b1cee1e68f929e57a8f3be0ad4e97
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-27 22:04:19 +0800

    Update battle.js

--- modules/battle.js
+++ modules/battle.js
@@ -324,7 +324,7 @@ function battlesys(command,move,target,commander){
 					\n--------------------\
 					\n你可以:\
 					\n 1.通常攻擊\n';
-			
+			console.log(info[3][info[9]].Skill);
 			for(var i = 0;i<3;i++){
 				if(info[3][info[9]].Skill[i] != '無'){
 					say +=' ' +  (i+1) + '.' + info[3][info[9]].Skill[i] + '\n';
