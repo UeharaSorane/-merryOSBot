eeeacfb298d16c22c36dbb0a52faa4d2bc740a76
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-28 00:07:38 +0800

    Update Skill.js

--- database/Skill.js
+++ database/Skill.js
@@ -34,7 +34,7 @@ SkillDB.useServiceAccountAuth(creds, function (err) {
 					let Effect = rows[i].effect.split(';');
 					
 					for(var sp1 = 0; sp1<Effect.length;sp1++){
-						Skill[i].Effect[sp1] = Effect.split(',');
+						Skills[i].Effect[sp1] = Effect.split(',');
 					}
 					
 					
