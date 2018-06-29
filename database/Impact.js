ca8fde98c2779bc9c535a13ae0245d1253c68e16
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-06-29 23:14:47 +0800

    Update Impact.js

--- database/Impact.js
+++ database/Impact.js
@@ -1,1 +1,52 @@
 
+var GoogleSpreadsheet = require('google-spreadsheet');
+var creds = require('../client_secret.json');
+
+var SkillDB = new GoogleSpreadsheet('19dmO1oYtwVES_It0rixnrYVojvRMsbz9w5H3yg1Cm9Y');
+
+var Skills = [];
+
+SkillDB.useServiceAccountAuth(creds, function (err) {
+		
+ 
+	
+ // 是先將資料讀進陣列
+	SkillDB.getRows(4 , 
+		function (err, rows) {
+			if (err) {
+				console.log( err );
+			}else{
+				for(var i=0; i< rows.length; i++){
+					Skills[i] = {};
+					
+					Skills[i].Name = rows[i].name;
+					Skills[i].Type = rows[i].type;
+					Skills[i].Mp = Number(rows[i].mp);
+					Skills[i].Range = rows[i].range;
+					Skills[i].con= [rows[i].effect.con1(','),rows[i].effect.con2(','),rows[i].effect.con3(',')];
+					Skills[i].Dmg = Number(rows[i].dmg);
+					Skills[i].Effect= rows[i].effect.split(';');
+					Skills[i].Description = rows[i].description;
+					Skills[i].Scrpit= rows[i].effect.scrpit('/');
+
+
+				}
+				//console.log(Skills);
+				console.log('必殺技資料 讀取完成');
+			}
+		
+
+			
+			});
+	
+		
+		
+	});
+
+function getImpactData(){
+	return Skills;
+}
+
+module.exports = {
+	getImpactData
+};
