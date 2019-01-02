fd446c1cde575c38a29aeab2e554a93106e5bddd
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2019-01-02 18:14:51 +0800

    Update NormalSys.js

--- modules/NormalSys.js
+++ modules/NormalSys.js
@@ -15,6 +15,8 @@ function NormalMode(mainMsg,trigger,UserID,UserN,GroupID) {
 	if (trigger.match(/^鸚鵡測試$/) != null) return exports.Testing.ReplyTest(UserN,mainMsg[1]);//測試reply功能用
 	if (trigger.match(/^大聲測試$/) != null) return exports.Testing.PushTest(UserID,GroupID);//測試push功能用
 	if (trigger.match(/^戰鬥系統測試$/) != null) return exports.Testing.SimBattle(UserID,GroupID,UserN);//測試開啟戰鬥系統功能用
+	if (trigger.match(/^1淦話$/) != null) return exports.Testing.ReplyTest(UserN,mainMsg[1]);//測試reply功能用
+
 }
 
 module.exports = {
